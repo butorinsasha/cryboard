e46616e6606f312ef82a653d3c443173ceb95d1a
a.b.butorin <a.b.butorin@tinkoff.ru>
2024-02-27 11:32:16 +0300

    cryboard

--- app/src/main/java/com/avinashdavid/ioskeyboardclone/MyInputMethodService.kt
+++ app/src/main/java/com/avinashdavid/ioskeyboardclone/MyInputMethodService.kt
@@ -4,14 +4,17 @@ import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
 import android.text.TextUtils
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 
+private const val KEY: Int = 1
 
 class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
     private var keyboardView: KeyboardView? = null
     private var keyboard: Keyboard? = null
     private var caps = false
+    private var cryptogram = charArrayOf()
 
     override fun onPress(p0: Int) {
     }
@@ -23,6 +26,7 @@ class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardAction
         val inputConnection = currentInputConnection
         if (inputConnection != null) {
             when (primaryCode) {
+
                 Keyboard.KEYCODE_DELETE -> {
                     val selectedText = inputConnection.getSelectedText(0)
                     if (TextUtils.isEmpty(selectedText)) {
@@ -34,28 +38,38 @@ class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardAction
                     keyboard!!.isShifted = caps
                     keyboardView!!.invalidateAllKeys()
                 }
+
                 Keyboard.KEYCODE_SHIFT -> {
                     caps = !caps
                     keyboard!!.isShifted = caps
                     keyboardView!!.invalidateAllKeys()
                 }
+
                 Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
                     KeyEvent(
                         KeyEvent.ACTION_DOWN,
                         KeyEvent.KEYCODE_ENTER
                     )
                 )
+
+                44 -> {
+                    Log.i(this.toString(), "cryptogram = $cryptogram")
+                    inputConnection.commitText(cryptogram.concatToString(), 1)
+                    cryptogram = charArrayOf()
+                }
+
                 else -> {
                     var code = primaryCode.toChar()
                     if (Character.isLetter(code) && caps) {
                         code = Character.toUpperCase(code)
                     }
-                    inputConnection.commitText(code.toString(), 1)
+                    val encryptedCode = encryptChar(code, KEY)
+                    this.cryptogram = cryptogram.plus(encryptedCode)
                 }
             }
         }
-
     }
+
 
     override fun onText(p0: CharSequence?) {
     }
@@ -80,5 +94,18 @@ class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardAction
             setOnKeyboardActionListener(this@MyInputMethodService)
         }
         return keyboardView!!
+    }
+
+    private fun encryptMessage(message: String, key: Int): String {
+        val messageCharArray = message.toCharArray()
+        val cryptogramCharArray = charArrayOf()
+        for (ch in messageCharArray) {
+            cryptogramCharArray.plus(ch + key)
+        }
+        return cryptogramCharArray.toString()
+    }
+
+    private fun encryptChar(char: Char, key: Int): Char {
+        return char + key
     }
 }
